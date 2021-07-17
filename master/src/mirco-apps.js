be9228886fc92a85542203b01053f0d12105d56d
soupjian <soupjian@163.com>
2021-07-17 18:56:27 +0800

    feat:实现react微应用

--- master/src/mirco-apps.js
+++ master/src/mirco-apps.js
@@ -11,6 +11,12 @@ const microApps = [
     container: '#container',
     activeRule: genActiveRule("/umi")
   },
+  {
+    name: "react",
+    entry: process.env.NODE_ENV === 'production' ? 'http://192.168.164.129:3000' : '//localhost:3000',
+    container: '#container',
+    activeRule: genActiveRule("/react")
+  },
 ]
 
 function genActiveRule(routerPrefix) {
