bb61e1a3ccb8a197815611c85ddb5aca003ef8b9
soupjian <soupjian@163.com>
2021-07-17 17:20:15 +0800

    feat:实现vue微应用配置

--- master/src/mirco-apps.js
+++ master/src/mirco-apps.js
@@ -3,7 +3,7 @@ const microApps = [
     name: "user",
     entry: process.env.NODE_ENV === 'production' ? 'http://192.168.164.129:8081' : '//localhost:8081',
     container: '#container',
-    activeRule: genActiveRule("/user")
+    activeRule: genActiveRule("/vue")
   },
 ]
 
